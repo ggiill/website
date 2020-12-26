222aeae2e3327a4d07b80665a37eb82beae5c91f
gturetsky <26069404+gturetsky@users.noreply.github.com>
2020-12-25 23:12:31 -0500

    changing meta to no-cache

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 hugo # build the site and places into /public
-gsutil -m rsync -R -d public/ gs://www.gil.fyi # syncs to GCS
+gsutil -m rsync -r -d public/ gs://www.gil.fyi # syncs to GCS
 gsutil iam ch allUsers:objectViewer gs://www.gil.fyi # makes all GCS objctes public
 # set no caching:
-gsutil setmeta -h "Content-Type:text/html" \
-  -h "Cache-Control:private, max-age=0, no-transform" \
-  gs://www.gil.fyi/*.html
+gsutil -m setmeta -r -h "Content-Type:text/html" \
+  -h "Cache-Control:no-cache, max-age=0" \
+  gs://www.gil.fyi
