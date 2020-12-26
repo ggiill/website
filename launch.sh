d98025c35d35171d91c7e50c64adf775f15f0032
gturetsky <26069404+gturetsky@users.noreply.github.com>
2020-12-26 10:33:54 -0500

    ignoring .DS_Store in gsutil rsync

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 hugo --cleanDestinationDir # build the site and places into /public
-gsutil -m rsync -r -d public/ gs://www.gil.fyi # syncs to GCS
+gsutil -m rsync -r -d -x ".*\.DS_Store$" public/ gs://www.gil.fyi # syncs to GCS
 gsutil iam ch allUsers:objectViewer gs://www.gil.fyi # makes all GCS objctes public
 # set no caching:
 gsutil -m setmeta -r -h "Content-Type:text/html" \
