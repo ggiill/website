e80c8b04aa9817a08a492ef9d0531e92740ea279
gturetsky <26069404+gturetsky@users.noreply.github.com>
2020-12-28 21:12:41 -0500

    tweaks to footer

--- launch.sh
+++ launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "**BUILDING SITE**"
 hugo --cleanDestinationDir # build the site and places into /public
+echo "**SYNCING TO GCS**"
 gsutil -m rsync -r -d -x ".*\.DS_Store$" public/ gs://www.gil.fyi # syncs to GCS
 gsutil iam ch allUsers:objectViewer gs://www.gil.fyi # makes all GCS objctes public
 # set no caching:
@@ -9,3 +11,4 @@ gsutil -m setmeta -r -h "Content-Type:text/html" \
 gsutil -m setmeta -r -h "Content-Type:text/css" \
   -h "Cache-Control:no-cache, max-age=0" \
   gs://www.gil.fyi/**.css
+echo "**DONE**"
